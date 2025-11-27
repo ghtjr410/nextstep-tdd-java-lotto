bad46cb69194a8f666b5980c75ae45950bb8aa01
ghtjr410 <ghtjr410@gmail.com>
2025-11-27 19:53:11 +0900

    refactor: Operation을 record로 변환하여 불변성 및 보일러플레이트 개선
    Operation.java
    - Operation 클래스를 record로 전환
    - 불필요한 필드 선언 및 생성자 제거
    - apply 로직은 기존대로 유지

--- src/main/java/caculator/domain/Operation.java
+++ src/main/java/caculator/domain/Operation.java
@@ -1,16 +1,9 @@
 package caculator.domain;
 
-public class Operation {
-    private final Operator operator;
-    private final Operand operand;
+public record Operation(Operator operator, Operand operand) {
 
     public Operation(String operator, String operand) {
         this(Operator.from(operator), new Operand(operand));
-    }
-
-    public Operation(Operator operator, Operand operand) {
-        this.operator = operator;
-        this.operand = operand;
     }
 
     public Operand apply(Operand currentValue) {
